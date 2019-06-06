251eef48efda9143af246b021e9e37134a251867
christurgeon <turgeonchris3@outlook.com>
2019-06-05 22:40:40 -0400

    Successfully have API access

--- Unlist/src/GetEmails.py
+++ Unlist/src/GetEmails.py
@@ -1,59 +1,125 @@
-import requests
-import imaplib
-#from apiclient import errors
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from apiclient import errors
+import base64
+import email
+import pickle
+import os.path
 
-"""
-def DeleteMessage(service, user_id, msg_id):
 
-  This method deletes a specific email message.
+# =============================================================================
+# =============================================================================
+
+
+def GetMessageWithId(service, user_id, msg_id):
+  """Get a Message with given ID.
 
   Args:
     service: Authorized Gmail API service instance.
     user_id: User's email address. The special value "me"
-             can be used to indicate the authenticated user.
-    msg_id: ID of Message to delete.
+    can be used to indicate the authenticated user.
+    msg_id: The ID of the Message required.
 
+  Returns:
+    A Message.
+  """
   try:
-    service.users().messages().delete(userId=user_id, id=msg_id).execute()
-    print("Message with id:", msg_id, "deleted successfully.")
-  except errors.HttpError, error:
-    print("An error occurred:", error)
-"""
+    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+    print("Message snippet: %s" % message['snippet'])
+    return message
+  except errors.HttpError as error:
+    print("An error occurred: %s" % error)
 
-def login_with_cached_url(gmail):
-    try:
-        url = "https://mail.google.com/mail/u/?authuser={}".format(gmail)
-    except:
-        print("An error occurred")
+
+# =============================================================================
+# =============================================================================
+
+
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+  """List all Messages of the user's mailbox with label_ids applied.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_ids: Only return Messages with these labelIds applied.
+
+  Returns:
+    List of Messages that have all required Labels applied. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate id to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               labelIds=label_ids).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id,
+                                                 labelIds=label_ids,
+                                                 pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError as error:
+    print('An error occurred: %s' % error)
+
+
+# =============================================================================
+# =============================================================================
+
+
+def configure(SCOPES, CREDENTIALS_PATH):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_PATH, SCOPES)
+            creds = flow.run_console()
+        # Save the credentials for the next run
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    service = build("gmail", "v1", credentials=creds)
+
+    # Call the Gmail API
+    results = service.users().labels().list(userId="me").execute()
+    labels = results.get("labels", [])
+
+    if not labels:
+        print("No labels found.")
+    else:
+        print("Labels:")
+        for label in labels:
+            print(label["name"])
+    return service
+
+
+# =============================================================================
+# =============================================================================
 
 
 if __name__ == "__main__":
 
-    FIRST = "RCOS"
-    LAST = "Unlist"
     GMAIL = "unlist.RCOS@gmail.com"
     PASSWORD = "RCOS_unlist!"
+    API_KEY = "AIzaSyA5GIhpj5vLQrOl6jdkSZl5ao8yNakvMSA"
 
-"""
-    SMTP_SERVER = "imap.gmail.com"
-    SMTP_PORT = 993
-    EMAIL_ADDRESS =
-    EMAIL_PASSWORD =
-
-    # Use imap module to connect to SMTP sever over SSL
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    except Exception as e:
-        print("Error trying to login to", EMAIL_ADDRESS)
-
-    # Go to inbox and get list of ids for each email in the account
-    try:
-        mail.select("inbox")
-        type, data = mail.search(None, "ALL")
-        mail_ids = data[0]
-        id_list = mail_ids.split()
-        all_email_ids = [int(id) for id in id_list]
-    except Exception as e:
-        print(str(e))
-"""
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+    CREDENTIALS_PATH = "../../credentials.json"
+    service = configure(SCOPES, CREDENTIALS_PATH)
+    GetMessageWithId(service, "me", "<CA+dP_+1UAVfMJ1AjQOtstitoM4ZdMEKMqgq9cL2BMJxgLFbw7g@mail.gmail.com>")
