a6ecb87f25be6fb007c1b06b31a6a8f3b0889bcc
markc373 <markchin373@gmail.com>
2019-06-18 14:43:43 -0400

    Added check box list functionality, reading in from PY script

--- Unlist/Form1.Designer.cs
+++ Unlist/Form1.Designer.cs
@@ -33,13 +33,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.Submit = new System.Windows.Forms.Button();
-            this.Company1 = new System.Windows.Forms.CheckedListBox();
+            this.UnsubList = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.unsubscribe = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // userName
             // 
             this.userName.Location = new System.Drawing.Point(68, 55);
-            this.userName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.userName.Margin = new System.Windows.Forms.Padding(4);
             this.userName.Name = "userName";
             this.userName.Size = new System.Drawing.Size(244, 25);
             this.userName.TabIndex = 0;
@@ -48,7 +50,7 @@
             // password
             // 
             this.password.Location = new System.Drawing.Point(68, 126);
-            this.password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.password.Margin = new System.Windows.Forms.Padding(4);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(244, 25);
             this.password.TabIndex = 3;
@@ -81,7 +83,7 @@
             // 
             this.Submit.Font = new System.Drawing.Font("AcmeFont", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Submit.Location = new System.Drawing.Point(356, 55);
-            this.Submit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Submit.Margin = new System.Windows.Forms.Padding(4);
             this.Submit.Name = "Submit";
             this.Submit.Size = new System.Drawing.Size(250, 100);
             this.Submit.TabIndex = 6;
@@ -89,29 +91,49 @@
             this.Submit.UseVisualStyleBackColor = true;
             this.Submit.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // Company1
+            // UnsubList
             // 
-            this.Company1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Company1.FormattingEnabled = true;
-            this.Company1.Location = new System.Drawing.Point(68, 250);
-            this.Company1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Company1.Name = "Company1";
-            this.Company1.Size = new System.Drawing.Size(613, 274);
-            this.Company1.TabIndex = 7;
+            this.UnsubList.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UnsubList.FormattingEnabled = true;
+            this.UnsubList.Location = new System.Drawing.Point(68, 250);
+            this.UnsubList.Margin = new System.Windows.Forms.Padding(4);
+            this.UnsubList.Name = "UnsubList";
+            this.UnsubList.Size = new System.Drawing.Size(613, 274);
+            this.UnsubList.TabIndex = 7;
+            this.UnsubList.SelectedIndexChanged += new System.EventHandler(this.Company1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(68, 552);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(613, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // unsubscribe
+            // 
+            this.unsubscribe.Location = new System.Drawing.Point(714, 250);
+            this.unsubscribe.Name = "unsubscribe";
+            this.unsubscribe.Size = new System.Drawing.Size(225, 76);
+            this.unsubscribe.TabIndex = 9;
+            this.unsubscribe.Text = "Unsubscribe from selection!";
+            this.unsubscribe.UseVisualStyleBackColor = true;
+            this.unsubscribe.Click += new System.EventHandler(this.Unsubscribe_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1026, 687);
-            this.Controls.Add(this.Company1);
+            this.Controls.Add(this.unsubscribe);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.UnsubList);
             this.Controls.Add(this.Submit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.password);
             this.Controls.Add(this.userName);
             this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Unlist";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -127,7 +149,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Submit;
-        private System.Windows.Forms.CheckedListBox Company1;
+        private System.Windows.Forms.CheckedListBox UnsubList;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button unsubscribe;
     }
 }
 
--- Unlist/Form1.cs
+++ Unlist/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ namespace Unlist
 {
     public partial class Form1 : Form
     {
+        public object Items { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -41,7 +45,44 @@ namespace Unlist
         {
             string user = userName.Text;
             string pass = password.Text;
-            Console.WriteLine(user);
+            System.IO.File.WriteAllText(@"D:\cs\Unlist\Unlist\src\credentials\credentials.txt", user+":"+pass);
+            
+            var lines = File.ReadAllLines(@"D:\cs\Unlist\Unlist\src\unsubscribeCompany.txt");
+            for (var i = 0; i < lines.Length; i += 1)
+            {
+                var line = lines[i];
+                var parts = line.Split(':');
+                this.UnsubList.Items.Add(parts[0]);
+                // Process line
+            }
+            Console.Write("test");
+                
+        }
+        private void run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "my/full/path/to/python.exe";
+            start.Arguments = string.Format("{0} {1}", cmd, args);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+            }
+        }
+
+        private void Company1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void Unsubscribe_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
