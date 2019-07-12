4033d16b8c28b5476485f56ead71c425378e7d1e
christurgeon <turgeonchris3@outlook.com>
2019-07-12 14:46:36 -0400

    Obtained emails

--- Unlist/src/Unsubscribe.py
+++ Unlist/src/Unsubscribe.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     #try:
     # Asssumes that chromedriver.exe is placed in the current working directory
     driver = webdriver.Chrome(CHROMEDRIVER_PATH + "/chromedriver.exe")
+    driver.implicitly_wait(5)
 
     # Tell the driver to open up the webpage
     driver.get(GMAIL_LOGIN)
 
     # Sleep to ensure a clean open
-    time.sleep(1)
+    time.sleep(2)
 
     # Send the email to the login page
     driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(GMAIL)
@@ -31,12 +32,44 @@ if __name__ == "__main__":
     driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
     time.sleep(2)
 
-    # Accept automated browsing
+    # Accept automated browsing --- this still needs to be tested
+    """
     try:
         driver.find_element_by_xpath('//*[@id="identifierDone"]').click()
+    except:
+        print("Did not prompt user to accept automated browsing...")
+    try:
+        driver.find_element_by_xpath("//div[@role='link']").click()
+    except:
+        print("Did not prompt user for confirm email...")
+    """
 
-    print("Program complete...")
+    # Load up all of the emails
+    emails = driver.find_elements_by_xpath("//*[@class='yW']/span")
+    time.sleep(2)
+    print("Found", len(emails), "emails!")
+
+    # Create a dictionary to hold email sender names that have been found
+    #         KEY: sender (string)
+    #       VALUE: unsubscribe link (string)
+    # value is empty string "" if no link found
+    email_dict = {}
+    for email in emails:
+        sender = email.text
+
+        # Continue if an unsubscribe link has already been found
+        if (sender in email_dict) and (email_dict[sender] != ""):
+            continue
+
+        # If a new email is found, see if we can unsubscribe from it
+        if sender not in email_dict:
+            email.click()
+            #Span.Ca obtain the unsibscribe link
+
+
+        print(sender)
+
 
     # Close the driver
-    finally:
-        driver.close()
+    #finally:
+    #    driver.close()
